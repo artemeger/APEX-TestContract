a53a6a5a90d807ad382dd988ecc758f883600e35
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-07-28 18:15:16 +0200

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -1,21 +1,28 @@
 pragma solidity ^0.5.2;
 contract Lottery{
-    
+
     uint nonce;
-    
+
     function random() internal returns (uint) {
         uint randomNumber = uint(keccak256(abi.encodePacked(now, msg.sender, nonce))) % 1000;
         nonce++;
         return randomNumber;
     }
-    
-    function getTicket(uint _number) public payable returns (bool) {
+
+    function getTicket(uint _number) public payable returns (uint) {
       uint newRandom = random();
-      if(_number == newRandom && msg.value >= 100 ether) {
-          msg.sender.transfer(100000 ether);
-          return true;
+      if(msg.value >= 100 ether && _number >= 0 && _number < 1000) {
+          if(_number == newRandom){
+            msg.sender.transfer(200000 ether);
+          } else if ((_number <= newRandom + 5) && (_number >= newRandom - 5)){
+            msg.sender.transfer(50000 ether);
+          } else if ((_number <= newRandom + 25) && (_number >= newRandom - 25)){
+            msg.sender.transfer(5000 ether);
+          } else if ((_number <= newRandom + 150) && (_number >= newRandom - 150)){
+            msg.sender.transfer(100 ether);
+          }
       }
-      return false;
+      return newRandom;
     }
-   
+
 }
